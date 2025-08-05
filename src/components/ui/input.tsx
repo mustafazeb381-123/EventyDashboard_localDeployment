afefb88d45b91a7c1cf901b398fbf054c86526f2
yusufsoliman1231 <yusufsoliman1231@gmail.com>
2025-08-05 16:29:18 +0300

    before-the-pull

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -8,7 +8,7 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
       type={type}
       data-slot="input"
       className={cn(
-        "font-poppins file:text-foreground placeholder:text-muted-foreground bg-transparent selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border border-gray-400 hover:border-black flex h-9 w-full min-w-0 rounded-[16px] bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow,border] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "font-poppins file:text-foreground placeholder:text-muted-foreground h-[48px]  selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border border-gray-400 hover:border-black flex w-full min-w-0 rounded-[16px] bg-transparent px-3 text-base shadow-xs transition-[color,box-shadow,border] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
         "focus-visible:border-ring focus-visible:ring-ring focus-visible:ring-1 focus-visible:ring-offset-0 focus-visible:ring-offset-background aria-invalid:ring-2 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive hover:brightness-110 active:transition-none",
         "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
         className
