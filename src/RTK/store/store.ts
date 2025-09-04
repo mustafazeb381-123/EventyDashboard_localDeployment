93a225ecef3f062ebe2ce89c0eaf73a5f19d47fb
mustafazeb381-123 <mustafazeb381@gmail.com>
2025-09-04 11:31:48 +0300

    redux tookit added

---
+++ src/RTK/store/store.ts
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit'
+import eventReducer from '../features/event/eventSlice';
+
+export const store = configureStore({
+    reducer: {
+        event: eventReducer, // add other reducers here if needed
+
+    },
+})
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
--- src/main.tsx
+++ src/main.tsx
@@ -2,22 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-// src/main.jsx
 import "./styles/fonts.css"; // 👈 Import font definitions
 
-import '../src/utils/i18n'; // Import i18n configuration
-import { AuthProvider } from './context/AuthContext.tsx';
-document.documentElement.dir = localStorage.getItem('i18nextLng') === 'ar' ? 'rtl' : 'ltr';
+import { Provider } from "react-redux";
+import { store } from "./RTK/store/store.ts";
+import { AuthProvider } from "./context/AuthContext.tsx";
 import "../src/utils/i18n"; // Import i18n configuration
-document.documentElement.dir =
-  localStorage.getItem("i18nextLng") === "ar" ? "rtl" : "ltr";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider>
-
-    <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </AuthProvider>
-  </StrictMode>,
-)
-
+  </StrictMode>
+);
